9615eee7a454f2e3f7eb0326c25d8fdef77e070d
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-17 16:59:11 +0300

    first graphql server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+// construct a schema, using graphql schema language
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+// the root providers a resolver function for each API endpoint
+const root = {
+  hello: () => "Hello, World!"
+};
+
+// express server
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+
+app.listen(4000, () =>
+  console.log("Running a graphql API server at http://localhost:4000/graphql")
+);
